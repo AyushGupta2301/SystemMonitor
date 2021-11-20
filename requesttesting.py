a964ca894b60247397f8da0439e0485ae17580fd
AyushGupta2301 <ayush2301ak@gmail.com>
2021-11-20 12:28:37 +0530

    temp

--- requesttesting.py
+++ requesttesting.py
@@ -1,8 +1,9 @@
 import requests
 text = '<b>sampletext</b>' + str(66)
-paramperson = {'chat_id':'1158109698','text':text,'parse_mode':'HTML'}
-paramgroup = {'chat_id':'-761074619','text':'hello rohil billam!!!'}
+paramperson = {'chat_id':'<>','text':text,'parse_mode':'HTML'}
+paramgroup = {'chat_id':'<>','text':'hello rohil billam!!!'}
 
-r1 = requests.post('https://api.telegram.org/bot2133097358:AAFLewQeNHQysJS6sOIJ0L1ZMRuSX-rNTc8/sendMessage',params=paramperson)
+r1 = requests.post('https://api.telegram.org/bot<>/sendMessage',params=paramperson)
+# Removed ID and token for security reasons
 
 print(r1.json())
